06bce16c72c77bf09ea63992b32a5d0f243e8432
afistapratama12 <pratama.otori.12@gmail.com>
2023-03-20 09:26:02 +0700

    fixing swagger and db

--- app.js
+++ app.js
@@ -6,15 +6,17 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const cors = require('cors')
+const path = require('path')
 
 const errorHandler = require('./middlewares/errorHandler')
 const Controller = require("./controllers/controller")
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(express.static('docs'))
 
 const swaggerUI = require('swagger-ui-express')
-  swaggerDocument = require('./swagger.json')
+  swaggerDocument = require(path.join(__dirname, 'docs', 'swagger.json'))
 
 app.options('*', cors())
 
@@ -22,7 +24,7 @@ app.post('/register', Controller.register)
 app.post('/login', Controller.login)
 app.get('/validate-token/:token', Controller.validateToken)
 
-app.get('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.get("/", (req, res, next) => {
   res.send("Auth API Running Successfully")
